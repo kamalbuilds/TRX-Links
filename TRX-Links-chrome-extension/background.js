9fb10822cefd9884f20c766b26cdf7af07c4c143
kamalbuilds <geniusamansingh@gmail.com>
2024-10-08 17:49:51 +0530

    update extension

--- TRX-Links-chrome-extension/background.js
+++ TRX-Links-chrome-extension/background.js
@@ -5,5 +5,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       .then(data => sendResponse(data))
       .catch(error => sendResponse({ error: error.message }));
     return true;  // Will respond asynchronously
+  } else if (request.action === 'sendTransaction') {
+    fetch('https://api.trongrid.io/wallet/broadcasttransaction', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(request.transaction),
+    })
+    .then(response => response.json())
+    .then(result => sendResponse({ result }))
+    .catch(error => sendResponse({ error: error.message }));
+    return true;  // Will respond asynchronously
   }
 });
